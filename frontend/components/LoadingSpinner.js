4fd2eb1f5caabe750006e62da092bd277ccb0fa9
NordBaltic <info@litexus.lt>
2025-02-26 01:22:26 +0200

    Create LoadingSpinner.js

---
+++ frontend/components/LoadingSpinner.js
@@ -0,0 +1,10 @@
+// 📂 /frontend/components/LoadingSpinner.js - Užkrovimo animacija
+import '../styles.css';
+
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
