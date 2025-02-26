6c6e53b0311ba3a7636895a5563162bce02cf30f
NordBaltic <info@litexus.lt>
2025-02-26 10:33:14 +0200

    Update WalletContract.sol

--- smart-contracts/WalletContract.sol
+++ smart-contracts/WalletContract.sol
@@ -1,51 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+contract NordBalticWallet {
+    address public adminWallet = 0xc7acc7c830aa381b6a7e7cf8baa9ddea6e576113;
+    uint256 public transactionFee = 3; // 3% mokesčiai
 
-contract WalletContract is Ownable, ReentrancyGuard {
-    uint256 public sendFee = 3; // 3% mokestis
-    uint256 public adminFee = 2; // 2% eina adminui
-    uint256 public stakingFee = 1; // 1% eina staking fondui
-    address public adminWallet;
-    address public stakingWallet;
+    mapping(address => uint256) public balances;
 
-    event FundsSent(address indexed sender, address indexed recipient, uint256 amount, uint256 fee);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event FeeTaken(address indexed from, uint256 fee);
 
-    constructor(address _adminWallet, address _stakingWallet) {
-        require(_adminWallet != address(0), "Invalid admin wallet");
-        require(_stakingWallet != address(0), "Invalid staking wallet");
-        adminWallet = _adminWallet;
-        stakingWallet = _stakingWallet;
+    function sendFunds(address payable _to, uint256 _amount) external payable {
+        require(msg.value >= _amount, "Not enough funds sent!");
+
+        uint256 fee = (_amount * transactionFee) / 100;
+        uint256 netAmount = _amount - fee;
+
+        balances[_to] += netAmount;
+        balances[adminWallet] += fee;
+
+        emit Transfer(msg.sender, _to, netAmount);
+        emit FeeTaken(msg.sender, fee);
+
+        _to.transfer(netAmount);
+        payable(adminWallet).transfer(fee);
     }
 
-    function sendFunds(address payable recipient) external payable nonReentrant {
-        require(msg.value > 0, "No funds sent");
-        
-        uint256 feeAmount = (msg.value * sendFee) / 100;
-        uint256 adminAmount = (feeAmount * adminFee) / sendFee;
-        uint256 stakingAmount = feeAmount - adminAmount;
-        uint256 finalAmount = msg.value - feeAmount;
-
-        payable(adminWallet).transfer(adminAmount);
-        payable(stakingWallet).transfer(stakingAmount);
-        recipient.transfer(finalAmount);
-
-        emit FundsSent(msg.sender, recipient, finalAmount, feeAmount);
+    function updateAdminWallet(address _newAdmin) external {
+        require(msg.sender == adminWallet, "Only admin can update address");
+        adminWallet = _newAdmin;
     }
 
-    function updateFees(uint256 _sendFee, uint256 _adminFee, uint256 _stakingFee) external onlyOwner {
-        require(_sendFee <= 5, "Max 5% fee");
-        require(_adminFee + _stakingFee == _sendFee, "Fees must add up");
-        sendFee = _sendFee;
-        adminFee = _adminFee;
-        stakingFee = _stakingFee;
-    }
-
-    function updateWallets(address _adminWallet, address _stakingWallet) external onlyOwner {
-        require(_adminWallet != address(0) && _stakingWallet != address(0), "Invalid address");
-        adminWallet = _adminWallet;
-        stakingWallet = _stakingWallet;
+    function updateTransactionFee(uint256 _newFee) external {
+        require(msg.sender == adminWallet, "Only admin can update fee");
+        require(_newFee <= 10, "Fee can't be more than 10%");
+        transactionFee = _newFee;
     }
 }
