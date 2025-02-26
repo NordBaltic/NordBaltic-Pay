42df39d2dc1d79edcd8dcb6c7d7e269e71216e15
NordBaltic <info@litexus.lt>
2025-02-26 11:12:35 +0200

    Update swap.js

--- frontend/pages/swap.js
+++ frontend/pages/swap.js
@@ -1,11 +1,6 @@
-// 📂 /pages/swap.js - Swap Puslapis (nukreipia į komponentą)
+// 📂 /pages/swap.js
 import Swap from "../components/Swap";
 
 export default function SwapPage() {
-  return (
-    <div>
-      <h1 className="page-title">🔄 Swap Crypto</h1>
-      <Swap />
-    </div>
-  );
-}
+  return <Swap />;
+    }
