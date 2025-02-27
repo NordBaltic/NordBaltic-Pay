a905445a8a72a62653c97c048d1cc718d509b82d
NordBaltic <info@litexus.lt>
2025-02-27 06:18:15 +0200

    Delete pages directory

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css"; // ✅ Turi būti frontend/styles/globals.css
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css"; // ✅ Turi būti frontend/styles/Home.module.css
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>Welcome to NordBaltic Pay</h1>
-    </div>
-  );
-}
