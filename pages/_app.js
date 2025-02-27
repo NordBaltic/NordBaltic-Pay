2601020d067b99e7169056edfabb80fe1273eaea
NordBaltic <info@litexus.lt>
2025-02-28 00:54:02 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'; // Importuoja globalų CSS
+import '../styles/theme.css'; // Importuoja theme CSS
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
