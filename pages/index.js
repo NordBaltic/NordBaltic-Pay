1d3efe55f6b00db14ac6581787d73141ecf54998
NordBaltic <info@litexus.lt>
2025-02-27 04:07:06 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
-// pages/index.js
 export default function Home() {
-  return <h1>NordBaltic Pay Home Page</h1>;
+  return (
+    <div className="container">
+      <div className="glass-card">
+        <h1>NordBaltic Pay</h1>
+        <p>Pasaulinė Web3 finansų ekosistema</p>
+        <a href="/dashboard" className="button">Pradėti</a>
+      </div>
+    </div>
+  );
 }
