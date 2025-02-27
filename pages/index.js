91511f49702159d2aff80a7e03711576c54df5f7
NordBaltic <info@litexus.lt>
2025-02-28 01:23:25 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,10 @@ export default function Home() {
         <a href="#">Settings</a>
       </nav>
 
-      <div className="glass fade-in glowing-border card">
-        <h1 className="neon-text gold-text">NordBaltic Pay ULTRA PREMIUM UI</h1>
-        <button className="smooth-button">Let's Go</button>
+      <div className="glass fade-in card">
+        <h1 className="gold-text">NordBaltic Pay ULTRA PREMIUM UI</h1>
+        <button className="button">Let's Go</button>
       </div>
-
-      <div className="loader"></div>
     </div>
   );
 }
