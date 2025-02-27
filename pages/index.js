14e2ab9d20e9a9decbe268591549d99625c4101e
NordBaltic <info@litexus.lt>
2025-02-28 00:25:55 +0200

    Create index.tsx

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>NordBaltic Pay - BSC Wallet</h1>
+      <p>Švari pradžia! Čia kursime geriausią Web3 finansinę sistemą.</p>
+    </div>
+  );
+}
