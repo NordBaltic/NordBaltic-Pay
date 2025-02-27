d7014b87ea34854b17065c465cb4b42e0537935d
NordBaltic <info@litexus.lt>
2025-02-27 06:24:42 +0200

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const bodyBg = getComputedStyle(document.body).backgroundColor;
+    console.log("✅ `globals.css` įkeltas. Body background spalva:", bodyBg);
+  }, []);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
