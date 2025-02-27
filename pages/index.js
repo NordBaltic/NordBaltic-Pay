db848daf054a43f5397de944015f77917557bad0
NordBaltic <info@litexus.lt>
2025-02-28 01:04:37 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="container">
+    <div className="center-container">
       <div className="glass fade-in">
         <h1 className="neon-text">NordBaltic Pay ULTRA PREMIUM UI</h1>
         <button className="button">Let's Go</button>
