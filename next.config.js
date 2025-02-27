ef55695de4f3733e7da01c1a431cc664e85ff2af
NordBaltic <info@litexus.lt>
2025-02-27 03:56:56 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://api.nordbalticpay.com",
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+  },
+  images: {
+    domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"]
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": require("path").resolve(__dirname),
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
