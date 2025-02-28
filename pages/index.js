6cea168c5af1bb16e38105317f8bf0ed8527bf60
NordBalticum STAFF <info@litexus.lt>
2025-02-28 17:10:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 export default function Home() {
   return (
     <div className="login-container">
-      <h1 className="gold-text">Welcome to NordBaltic Pay</h1>
-      <div className="login-box">
-        <h2>Sign in to your wallet</h2>
+      <h1 className="gold-text fade-in">Welcome to NordBaltic Pay</h1>
+      <div className="login-box glass-morph">
+        <h2 className="subheading">Sign in to your wallet</h2>
 
-        <button className="wallet-button">
+        <button className="wallet-button neon-glow">
           <img src="/icons/walletconnect.svg" className="button-icon" alt="WalletConnect" />
           Connect with WalletConnect
         </button>
 
-        <button className="email-button">
+        <button className="email-button neon-glow">
           <img src="/icons/email.svg" className="button-icon" alt="Email Login" />
           Login with Email
         </button>
+
+        <div className="login-footer">
+          <p className="info-text">Secure & Encrypted. Your keys, your crypto.</p>
+        </div>
       </div>
     </div>
   );
