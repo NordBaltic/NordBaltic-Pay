74a4c85f1b09823e50c87afe2f9d9da0e95e2c5e
NordBalticum STAFF <info@litexus.lt>
2025-02-28 18:02:57 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import '../styles/globals.css';
 import '../styles/theme.css';
-import '../styles/navbar.css';  
+import '../styles/navbar.css';
 import '../styles/buttons.css';
 import '../styles/tables.css';
 import '../styles/cards.css';
 import '../styles/inputs.css';
 import '../styles/loading.css';
 import '../styles/login.css';
-import '../styles/forms.css';     // ✅ Formų dizainas
-import '../styles/animations.css';  // ✅ Animacijos
+import '../styles/forms.css';      // ✅ Formų dizainas
+import '../styles/animations.css'; // ✅ Animacijos
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
