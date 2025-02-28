5ed40765bab4d6ef545131239dd7b281f6951849
NordBalticum STAFF <info@litexus.lt>
2025-02-28 05:36:46 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import '../styles/tables.css';
 import '../styles/cards.css';
 import '../styles/inputs.css';
 import '../styles/loading.css';
+import '../styles/login.css';  // ✅ PRIDĖTAS LOGIN UI
 import { playUISound } from '../utils/sounds';
 
 export default function MyApp({ Component, pageProps }) {
