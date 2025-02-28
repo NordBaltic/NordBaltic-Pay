45eb4cc919437558a812a5fdf44dfdd8a072b327
NordBaltic <info@litexus.lt>
2025-02-28 05:21:14 +0200

    Update sounds.js

--- utils/sounds.js
+++ utils/sounds.js
@@ -1,9 +1,13 @@
 export function playUISound() {
-  const sound = new Audio('/sounds/click.mp3');
-  sound.volume = 0.5;
-  sound.play();
+  if (typeof document !== "undefined") {  // ✅ FIX: Patikrina, ar `document` egzistuoja
+    const sound = new Audio('/sounds/click.mp3');
+    sound.volume = 0.5;
+    sound.play();
+  }
 }
 
-document.querySelectorAll('.button, .navbar a').forEach((el) => {
-  el.addEventListener('click', playUISound);
-});
+if (typeof document !== "undefined") {  // ✅ FIX: Užtikrina, kad kodas neveiktų `server-side`
+  document.querySelectorAll('.button, .navbar a').forEach((el) => {
+    el.addEventListener('click', playUISound);
+  });
+}
